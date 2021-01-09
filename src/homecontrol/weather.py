937225069c3da5ee781d2f13c74c3ce8ca3453b7
Lex Brugman <lex@probaa.nl>
2021-01-09 23:38:59 +0100

    get weather data from home assistant

--- src/homecontrol/weather.py
+++ src/homecontrol/weather.py
@@ -1,4 +1,6 @@
 import logging
+import requests
+import settings
 
 from homecontrol.func_tools import return_cache
 
@@ -6,8 +8,25 @@ logger = logging.getLogger(__name__)
 
 
 @return_cache(refresh_interval=1800)
-def get_temperature_info(location):
-    high_temperature = -99
+def get_temperature_info():
+    url = f"{settings.HOMEASSISTANT_URL}/api/states/{settings.HOMEASSISTANT_WEATHER_ENTITY}"
+    headers = {
+        "Authorization": f"Bearer {settings.HOMEASSISTANT_TOKEN}",
+        "content-type": "application/json",
+    }
+
     current_temperature = -99
 
-    return round(high_temperature, 1), round(current_temperature, 1)
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            timeout=5,
+        )
+        if response.ok:
+            weather_data = response.json().get("attributes")
+            current_temperature = weather_data.get("temperature")
+    except Exception:
+        logger.exception("failed getting weather information")
+
+    return round(current_temperature, 1)
